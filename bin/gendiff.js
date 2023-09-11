2723dd43fb4ec48affc88272614cf44442b54a77
Екатерина Сайгы <katesaygi@gmail.com>
2023-09-11 20:19:17 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,10 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('0.8.0')
-    .helpOption('-h, --help', 'output usage information');
-
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format <type>', 'output format', 'stylish')
+    .argument('<filepath1>')
+    .argument('<filepath2>');
+    
     program.parse();
  
